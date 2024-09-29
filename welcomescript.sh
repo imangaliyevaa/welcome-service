1520b09ac1178115468ccfb9e2292bd3d3c48817
EC2 Default User <ec2-user@ip-172-31-10-11.us-east-2.compute.internal>
2024-09-29 00:50:10 +0000

    Add unnecessary changes to welcomescript.sh

--- welcomescript.sh
+++ welcomescript.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+<<<<<<< HEAD
 echo "Hello 312 Student!"
 echo "Enter Your Name Please:"
 read name
 echo "$name, welcome to 312 School"
+=======
+echo "Hello 312 Stadent!"
+echo "Inter Your Name Please:"
+read name
+echo "$names, welcome to 313 School"
+>>>>>>> d250795 (Script added)
+echo "new unnecessary changes"
+
